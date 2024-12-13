02434c484dc7840895a20c529e4340853a061f62
Marcelo <carneiromarcelo100@hotmail.com>
2024-12-12 21:17:59 -0300

    Criando o template HTML (EJS)

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const routes = require('./routes');
+const path = require('path');
+
 
 const app = express();
 
-// Rota raiz
-app.get('/', (req, res) => {
-    res.send('<h1>Está tudo ok! Agora sim!</h1>');
-})
 
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(routes);
 
 app.listen(3000, () => {
     console.log('Acessar: localhost:3000');
