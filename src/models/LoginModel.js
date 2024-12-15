40ae20c24d010a308355265cd183ff2fcfc6df3b
Marcelo <carneiromarcelo100@hotmail.com>
2024-12-15 13:19:12 -0300

    Logando o usuario

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -16,6 +16,24 @@ class Login {
     this.user = null;
   }
 
+  async login() {
+    this.valida();
+    if (this.errors.length > 0) return;
+
+    this.user = await LoginModel.findOne({ email: this.body.email });
+
+    if(!this.user){
+      this.errors.push('Usuário não encontrado');
+      return;
+    }
+
+    if(!bcryptjs.compareSync(this.body.senha, this.user.senha)){
+       this.errors.push('Senha inválida');
+       this.user = null;
+       return;
+    }
+  }
+
   async register() {
     this.valida();
     if (this.errors.length > 0) return;
@@ -27,11 +45,7 @@ class Login {
     const salt = bcryptjs.genSaltSync();
     this.body.senha = bcryptjs.hashSync(this.body.senha, salt);
 
-    try {
-      this.user = await LoginModel.create(this.body);
-    } catch (e) {
-      console.log(e);
-    }
+    this.user = await LoginModel.create(this.body);
   }
 
   async userExists() {
